38cbfa908db48cd1a32fb8381322582448cb8122
2u6z3r0 <samrat.patil0202@gmail.com>
2018-04-26 12:58:59 +0530

    added all required code for user registration, and ignored vscode files

--- exam_management/models.py
+++ exam_management/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Exam(models.Model):
+    exam_name = models.Charfield(max_lenght=100)
+
+class Question(models.Model):
+    question_text = models.Charfield(max_lenght=600)
+    option1 = models.Charfield(max_lenght=200)
+    option2 = models.Charfield(max_lenght=200)
+    option3 = models.Charfield(max_lenght=200)
+    option4 = models.Charfield(max_lenght=200)
+class Answer(models.Model):
+
+
