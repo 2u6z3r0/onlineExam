6074472fd193628ddecd41d6639fd13a15a76510
2u6z3r0 <samrat.patil0202@gmail.com>
2018-04-26 17:44:31 +0530

    created Exam related models

--- exam_management/models.py
+++ exam_management/models.py
@@ -1,16 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Exam(models.Model):
-    exam_name = models.Charfield(max_lenght=100)
+    exam_name = models.CharField(max_length=100)
+    total_marks = models.IntegerField()
+    created = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+
 
 class Question(models.Model):
-    question_text = models.Charfield(max_lenght=600)
-    option1 = models.Charfield(max_lenght=200)
-    option2 = models.Charfield(max_lenght=200)
-    option3 = models.Charfield(max_lenght=200)
-    option4 = models.Charfield(max_lenght=200)
-class Answer(models.Model):
+    exam = models.ForeignKey(Exam, on_delete=models.CASCADE)
+    question_text = models.CharField(max_length=600)
+    option1 = models.CharField(max_length=200)
+    option2 = models.CharField(max_length=200)
+    option3 = models.CharField(max_length=200)
+    option4 = models.CharField(max_length=200)
+    correct_option = models.CharField(max_length=200)
 
+class StudentExam(models.Model):
+    exam = models.ManyToManyField(Exam)
+    student = models.ManyToManyField(User)
+    completed = models.BooleanField(default=False)
 
+class StudentExamQA(models.Model):
+    student_exam = models.ManyToManyField(StudentExam)
+    question = models.ManyToManyField(Question)
+    answer = models.CharField(max_length=200)
+    is_correct = models.BooleanField(default=False)
+
+class Result(models.Model):
+    exam = models.ManyToManyField(Exam)
+    student = models.ManyToManyField(User)
+    score = models.IntegerField()
+    percentage = models.FloatField()
